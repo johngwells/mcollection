afbb5d75c0be4bd5894d6a32108fdb0735698846
John Wells <molekewl2017@gmail.com>
2020-03-03 15:07:19 -0800

    add api image to users

--- mcollection/src/components/card/card.component.jsx
+++ mcollection/src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import './card.styles.css';
 
 const Card = (props) => (
   <div className='card-container'>
+    <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set1&size=150x150`} />
     <h1>{props.monster.name}</h1>
   </div>
 );
