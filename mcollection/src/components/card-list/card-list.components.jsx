f86c879a3319e030b58e3404cfd5f357f2651d06
John Wells <molekewl2017@gmail.com>
2020-03-03 14:49:54 -0800

    moved .map over to the cardlist component

--- mcollection/src/components/card-list/card-list.components.jsx
+++ mcollection/src/components/card-list/card-list.components.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './card-list.styles.css';
+import "./card-list.styles.css";
 
-const CardList = props => {
-  console.log(props);
-  return <div className='card-list'>{props.children}</div>
-};
+const CardList = props => (
+  <div className="card-list">
+    {props.monsters.map(monster => (
+      <h1 key={monster.id}>{monster.name}</h1>
+    ))}
+  </div>
+);
 
 export default CardList;
