2b55741f97296b761dc9b625391417f1bcee8296
John Wells <molekewl2017@gmail.com>
2020-03-04 21:48:18 -0800

    destructure props

--- mcollection/src/components/card/card.component.jsx
+++ mcollection/src/components/card/card.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './card.styles.css';
 
-const Card = (props) => (
+const Card = ({ monster }) => (
   <div className='card-container'>
-    <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`} />
-    <h1>{props.monster.name}</h1>
-    <h2>{props.monster.email}</h2>
+    <img alt='monster' src={`https://robohash.org/${monster.id}?set=set1&size=180x180`} />
+    <h1>{monster.name}</h1>
+    <h2>{monster.email}</h2>
   </div>
 );
 
