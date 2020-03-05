f7723d6593cb6cd426111a027a99bb46e1db4a03
John Wells <molekewl2017@gmail.com>
2020-03-04 22:10:04 -0800

    adding search bar feature

--- mcollection/src/App.js
+++ mcollection/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,6 +22,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input 
+          type='search'
+          placeholder='search'
+          onChange={e => 
+            this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
