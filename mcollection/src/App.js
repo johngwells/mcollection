b3c97085e60acfbe54f8bbe37a317198001f3bc4
John Wells <molekewl2017@gmail.com>
2020-03-04 22:50:16 -0800

    moved search into new search bar component

--- mcollection/src/App.js
+++ mcollection/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import CardList from "./components/card-list/card-list.components.jsx";
+import { CardList }from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
 
 class App extends Component {
   constructor() {
@@ -24,12 +25,12 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search"
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
--- mcollection/src/components/card-list/card-list.component.jsx
+++ mcollection/src/components/card-list/card-list.component.jsx
@@ -4,12 +4,10 @@ import Card from '../card/card.component';
 
 import "./card-list.styles.css";
 
-const CardList = props => (
+export const CardList = props => (
   <div className="card-list">
     {props.monsters.map(monster => (
       <Card key={monster.id} monster={monster} />
     ))}
   </div>
 );
-
-export default CardList;
